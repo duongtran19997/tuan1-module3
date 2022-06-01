759012cfd4c06022264d14eae19ed6830da8986c
superman <duonga2qp@gmail.com>
2022-06-01 14:57:09 +0700

    nopai

---
+++ [Thuc-hanh]-Su-dung-axios,-lay-du-lieu-tu-API-bang-asyncawait/main.js
@@ -0,0 +1,12 @@
+let axios = require('axios')
+
+async function getJSONAsync() {
+    let json = await axios.get('http://jsonplaceholder.typicode.com/posts/1')
+    return json.data
+}
+
+getJSONAsync()
+    .then(result => {
+            console.log(result);
+        }
+    )
